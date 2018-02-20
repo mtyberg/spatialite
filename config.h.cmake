3d2bb017136ba81d3c897a13a8fd97ef660b648f
Mark Tyberg <mark@maptext.com>
2018-02-19 23:43:32 -0500

    using option,check_include_files and check_function_exists with config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,236 @@
+
+/* Should be defined in order to enable GCP support. */
+#cmakedefine ENABLE_GCP
+
+/* Should be defined in order to enable GeoPackage support. */
+#cmakedefine ENABLE_GEOPACKAGE
+
+/* Should be defined in order to enable LIBXML2 support. */
+#cmakedefine ENABLE_LIBXML2
+
+/* Should be defined in order to enable LWGEOM support. */
+#cmakedefine ENABLE_LWGEOM
+
+/* Should be defined in order to enable GEOS_ADVANCED support. */
+#cmakedefine GEOS_ADVANCED
+
+/* Should be defined in order to enable GEOS_TRUNK experimental support. */
+#cmakedefine GEOS_TRUNK
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#cmakedefine HAVE_FCNTL_H
+
+/* Define to 1 if you have the `fdatasync' function. */
+#cmakedefine HAVE_FDATASYNC
+
+/* Define to 1 if you have the <float.h> header file. */
+#cmakedefine HAVE_FLOAT_H
+
+/* Define to 1 if you have the <freexl.h> header file. */
+#cmakedefine HAVE_FREEXL_H
+
+/* Define to 1 if you have the `ftruncate' function. */
+#cmakedefine HAVE_FTRUNCATE
+
+/* Define to 1 if you have the <geos_c.h> header file. */
+#cmakedefine HAVE_GEOS_C_H
+
+/* Define to 1 if you have the `getcwd' function. */
+#cmakedefine HAVE_GETCWD
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#cmakedefine HAVE_GETTIMEOFDAY
+
+/* Define to 1 if you have the <iconv.h> header file. */
+#cmakedefine HAVE_ICONV_H
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <liblwgeom.h> header file. */
+#cmakedefine HAVE_LIBLWGEOM_H
+
+/* Define to 1 if you have the `sqlite3' library (-lsqlite3). */
+#cmakedefine HAVE_LIBSQLITE3
+
+/* Define to 1 if you have the `z' library (-lz). */
+#cmakedefine HAVE_LIBZ
+
+/* Define to 1 if you have the `localtime_r' function. */
+#cmakedefine HAVE_LOCALTIME_R
+
+/* Define to 1 if `lstat' has the bug that it succeeds when given the
+zero-length file name argument. */
+#cmakedefine HAVE_LSTAT_EMPTY_STRING_BUG
+
+/* Define to 1 if you have the <math.h> header file. */
+#cmakedefine HAVE_MATH_H
+
+/* Define to 1 if you have the `memmove' function. */
+#cmakedefine HAVE_MEMMOVE
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H
+
+/* Define to 1 if you have the `memset' function. */
+#cmakedefine HAVE_MEMSET
+
+/* Define to 1 if you have the <proj_api.h> header file. */
+#cmakedefine HAVE_PROJ_API_H
+
+/* Define to 1 if you have the <sqlite3ext.h> header file. */
+#cmakedefine HAVE_SQLITE3EXT_H
+
+/* Define to 1 if you have the <sqlite3.h> header file. */
+#cmakedefine HAVE_SQLITE3_H
+
+/* Define to 1 if you have the `sqrt' function. */
+#cmakedefine HAVE_SQRT
+
+/* Define to 1 if `stat' has the bug that it succeeds when given the
+zero-length file name argument. */
+#cmakedefine HAVE_STAT_EMPTY_STRING_BUG
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#cmakedefine HAVE_STDDEF_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#cmakedefine HAVE_STDIO_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#cmakedefine HAVE_STRCASECMP
+
+/* Define to 1 if you have the `strerror' function. */
+#cmakedefine HAVE_STRERROR
+
+/* Define to 1 if you have the `strftime' function. */
+#cmakedefine HAVE_STRFTIME
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H
+
+/* Define to 1 if you have the `strncasecmp' function. */
+#cmakedefine HAVE_STRNCASECMP
+
+/* Define to 1 if you have the `strstr' function. */
+#cmakedefine HAVE_STRSTR
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#cmakedefine HAVE_SYS_TIME_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#cmakedefine HAVE_ZLIB_H
+
+/* Define to 1 if `lstat' dereferences a symlink specified with a trailing
+slash. */
+#cmakedefine LSTAT_FOLLOWS_SLASHED_SYMLINK
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+*/
+#cmakedefine LT_OBJDIR
+
+/* Must be defined in order to disable debug mode. */
+#cmakedefine NDEBUG
+
+/* Should be defined in order to disable EPSG full support. */
+#cmakedefine OMIT_EPSG
+
+/* Should be defined in order to disable FREEXL support. */
+#cmakedefine OMIT_FREEXL
+
+/* Should be defined in order to disable GEOCALLBACKS support. */
+#cmakedefine OMIT_GEOCALLBACKS
+
+/* Should be defined in order to disable GEOS support. */
+#cmakedefine OMIT_GEOS
+
+/* Should be defined in order to disable ICONV support. */
+#cmakedefine OMIT_ICONV
+
+/* Should be defined in order to disable MATHSQL support. */
+#cmakedefine OMIT_MATHSQL
+
+/* Should be defined in order to disable PROJ.4 support. */
+#cmakedefine OMIT_PROJ
+
+/* Name of package */
+#cmakedefine PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#cmakedefine PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#cmakedefine PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#cmakedefine PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#cmakedefine PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#cmakedefine PACKAGE_URL
+
+/* Define to the version of this package. */
+#cmakedefine PACKAGE_VERSION
+
+/* Should be defined when linking liblwgeom from PostGIS 2.1 (or later). */
+#cmakedefine POSTGIS_2_1
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS
+
+/* Should contain a text-string describing the intended target CPU */
+#cmakedefine TARGET_CPU "@TARGET_CPU_STRING@"
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#cmakedefine TIME_WITH_SYS_TIME
+
+/* Define to 1 if your <sys/time.h> declares `struct tm'. */
+#cmakedefine TM_IN_SYS_TIME
+
+/* Version number of package */
+#cmakedefine VERSION "@VERSIONSTRING@"
+
+/* Must be =64 in order to enable huge-file support. */
+#cmakedefine _FILE_OFFSET_BITS
+
+/* Must be defined in order to enable huge-file support. */
+#cmakedefine _LARGEFILE_SOURCE
+
+/* Must be defined in order to enable huge-file support. */
+#cmakedefine _LARGE_FILE
+
+/* Define to empty if `const' does not conform to ANSI C. */
+#cmakedefine const
+
+/* Define to `long int' if <sys/types.h> does not define. */
+#cmakedefine off_t
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+#cmakedefine size_t
+
+/* Define to empty if the keyword `volatile' does not work. Warning: valid
+code using `volatile' can become incorrect without. Disable with care. */
+#cmakedefine volatile
